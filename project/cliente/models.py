0a058fe256b1c60eafda1cc531efa3fdd6742720
Carlos Tavares <carlos.tavaresa@outlook.com>
2023-12-06 16:23:47 -0400

    models created and functional

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    costo = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+    direccion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Reparacion(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    fecha_reparacion = models.DateField()
+    descripcion = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.servicio.nombre} para {self.cliente.nombre}"
